2f26db7ee0e5c329fc93ccc2b8a8db2cc8072f5c
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-14 12:36:18 +0000

    add title and elem

---
+++ src/Components/BudgetCard.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+import { currencyFormatter } from "../utils"
+
+const BudgetCard = ({name,amount,max}) => {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                  <div>{name}</div>
+                  <div>{currencyFormatter.format(amount)}/{currencyFormatter.format(max)}</div>
+                </Card.Title>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default BudgetCard
